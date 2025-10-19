a1b2c51726a0b83b2cc996d4d055c36096a18d5b
Colin <62516283+faletto@users.noreply.github.com>
2025-10-18 22:27:38 -0700

    Resolve backend merge conflicts

--- backend/apitest.py
+++ backend/apitest.py
@@ -1,3 +1,3 @@
 import requests
-a = requests.get("http://localhost:6767/api/generate_mcq", headers={"X-Topic":"Chocolate chip cookies","X-Num-Questions":"6"})
+a = requests.get("http://localhost:6767/api/get", headers={"X-Key":"40eb1348c81d41e1bdcda3eee5239cae","X-Num-Questions":"6"})
 print(a.content)
